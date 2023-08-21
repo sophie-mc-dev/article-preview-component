d111aa4f7cf517877fa018c105e877b02fb48dd9
Sofia Merino Costa <scosta.geprix@gmail.com>
2023-08-21 18:28:28 +0100

    readme update

--- main.js
+++ main.js
@@ -2,16 +2,8 @@ function displayPopUp() {
   const shareButton = document.getElementById("myShareButton");
   const popUp = document.getElementById("myPopUp");
 
-  shareButton.addEventListener("mouseover", () => {
-    popUp.classList.add("visible");
-  });
-
-  popUp.addEventListener("mouseout", () => {
+  shareButton.addEventListener("mouseout", () => {
     popUp.classList.remove("visible");
-  });
-
-  popUp.addEventListener("mouseover", () => {
-    popUp.classList.add("visible");
   });
 
   shareButton.addEventListener("click", () => {
